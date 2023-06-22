6bb346e367b1a6fd72cfc2d828bab8e25b11bf57
rafueru <rafael.albuquerque@gmail.com>
2023-06-22 18:50:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php
-include 'functions.php';
-// Your PHP code here.
-
-// Home Page template below.
-?>
-
-<?=template_header('Home')?>
-
-<div class="content">
-	<h2>Home</h2>
-	<p>Cadastro de alunos!</p>
-</div>
-
-<?=template_footer()?>
-
-<div class="content">
-    <h2>Cadastro de Alunos</h2>
-    <p>Use uma das opções abaixo para se cadastrar:</p>
-    <a href="auth-instagram.php">Login com Instagram</a>
-    <a href="auth-gmail.php">Login com Gmail</a>
-    <a href="auth-facebook.php">Login com Facebook</a>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gerenciamento de Alunos</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <div id="content"></div>
+</body>
+</html>
